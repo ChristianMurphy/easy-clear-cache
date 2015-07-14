ec8e2855ceb1c134703ed574db435fbcf584d169
Christian Murphy <Christian.Murphy.42@gmail.com>
2015-07-14 15:44:36 -0700

    resolve firefox icon issues

--- firefox/index.js
+++ firefox/index.js
@@ -3,14 +3,15 @@ const buttons = require('sdk/ui/button/action');
 const notifications = require('sdk/notifications');
 const {Cc, Ci} = require('chrome');
 const cache = Cc['@mozilla.org/network/cache-storage-service;1'].getService(Ci.nsICacheStorageService);
+const self = require('sdk/self');
 
 const button = new buttons.ActionButton({
     id: 'easy-clear-cache',
     label: 'Clear Cache',
     icon: {
-        16: './icon.png',
-        32: './icon.png',
-        64: './icon.png'
+        16: './icon-16.png',
+        32: './icon-32.png',
+        64: './icon-64.png'
     }
 });
 
@@ -19,6 +20,7 @@ button.on('click', () => {
 
     notifications.notify({
         title: 'Cache Cleared',
-        text: 'Refresh Page'
+        text: 'Refresh Page',
+        iconURL: './icon-64.png'
     });
 });
